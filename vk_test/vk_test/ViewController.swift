7e48cd4668005e2e0da3d95425e04f2c8fd40559
Kristofer Sartorial <kristofs@va-d3.kzn.21-school.ru>
2024-12-02 20:18:35 +0300

    alamofire is added

--- vk_test/vk_test/ViewController.swift
+++ vk_test/vk_test/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  vk_test
-//
-//  Created by Kristofer Sartorial on 12/2/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 

010720d666ef69783dcadcb0f6296583405f24e2
Kristofer Sartorial <kristofs@va-d3.kzn.21-school.ru>
2024-12-02 18:14:31 +0300

    start

---
+++ vk_test/vk_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk_test
+//
+//  Created by Kristofer Sartorial on 12/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
